99c31008ea74d4a744b4ad5461b7d7928fb02f01
Rob McClain <robmcclain@gmail.com>
2020-03-09 00:00:11 -0400

    Basic vehicle models

--- vehicles/models.py
+++ vehicles/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Make(models.Model):
+    name = models.CharField(max_length=100)
+
+class Model(models.Model):
+    name = models.CharField(max_length=100)
+    make = models.ForeignKey(Make, on_delete=models.CASCADE, related_name='models')
+
+class Trim(models.Model):
+    name = models.CharField(max_length=100)
+    model = models.ForeignKey(Model, on_delete=models.CASCADE, related_name='trims')
+    year = models.IntegerField()
