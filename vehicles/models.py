29e7163a8244d17d172aec1d64d76f957b06c14f
Rob McClain <robmcclain@gmail.com>
2020-03-09 21:47:24 -0400

    Add model string representations

--- vehicles/models.py
+++ vehicles/models.py
@@ -4,11 +4,20 @@ from django.db import models
 class Make(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 class Model(models.Model):
-    name = models.CharField(max_length=100)
     make = models.ForeignKey(Make, on_delete=models.CASCADE, related_name='models')
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return '{} {}'.format(self.make, self.name)
 
 class Trim(models.Model):
-    name = models.CharField(max_length=100)
     model = models.ForeignKey(Model, on_delete=models.CASCADE, related_name='trims')
+    name = models.CharField(max_length=100)
     year = models.IntegerField()
+
+    def __str__(self):
+        return '{} {} {}'.format(self.year, self.model, self.name)
