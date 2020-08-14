20cec9c53027bf1a6a9b86b56d62a5b80f79bc33
Rob McClain <robmcclain@gmail.com>
2020-08-13 20:43:15 -0400

    Use nested serializers for the vehicle foreign key fields

--- vehicles/serializers.py
+++ vehicles/serializers.py
@@ -10,18 +10,24 @@ class MakeSerializer(serializers.ModelSerializer):
 
 
 class ModelSerializer(serializers.ModelSerializer):
+    make = MakeSerializer()
+
     class Meta:
         model = Model
         fields = ['id', 'make', 'name']
 
 
 class ModelYearSerializer(serializers.ModelSerializer):
+    model = ModelSerializer()
+
     class Meta:
         model = ModelYear
         fields = ['id', 'model', 'year']
 
 
 class TrimSerializer(serializers.ModelSerializer):
+    model_year = ModelYearSerializer()
+
     class Meta:
         model = Trim
         fields = ['id', 'model_year', 'name']
