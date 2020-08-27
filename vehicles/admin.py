3e7e2318a74a019c8ed1691b18ae8747a01c5bfa
Rob McClain <robmcclain@gmail.com>
2020-08-26 20:52:11 -0400

    Add BodyStyle and DriveType to admin

--- vehicles/admin.py
+++ vehicles/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 
-from .models import Make, MediaLicense, Model, ModelYear, Trim
+from .models import BodyStyle, DriveType, Make, MediaLicense, Model, ModelYear, Trim
 
 
 admin.site.register(Make)
 admin.site.register(Model)
 admin.site.register(ModelYear)
 admin.site.register(Trim)
+admin.site.register(BodyStyle)
+admin.site.register(DriveType)
 admin.site.register(MediaLicense)
