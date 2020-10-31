4acad8ec6b4ede65768af0667b80db84ea0197dc
Rob McClain <robmcclain@gmail.com>
2020-10-31 00:07:42 -0400

    Add ModelYear properties for ranges of Trim field values

--- vehicles/models.py
+++ vehicles/models.py
@@ -53,6 +53,75 @@ class ModelYear(models.Model):
     def __str__(self):
         return '{} {}'.format(self.year, self.model)
 
+    @property
+    def available_drive_types(self):
+        return DriveType.objects.filter(trim__model_year=self).distinct().order_by('name')
+
+    @property
+    def seating_capacity_range(self):
+        seating_capacity_list = self.trims.filter(seating_capacity__isnull=False).values_list('seating_capacity', flat=True)
+        return (min(seating_capacity_list), max(seating_capacity_list)) if seating_capacity_list else (None, None)
+
+    @property
+    def mpge_city_range(self):
+        mpge_city_list = self.trims.filter(mpge_city__isnull=False).values_list('mpge_city', flat=True)
+        return (min(mpge_city_list), max(mpge_city_list)) if mpge_city_list else (None, None)
+
+    @property
+    def mpge_highway_range(self):
+        mpge_highway_list = self.trims.filter(mpge_highway__isnull=False).values_list('mpge_highway', flat=True)
+        return (min(mpge_highway_list), max(mpge_highway_list)) if mpge_highway_list else (None, None)
+
+    @property
+    def mpge_combined_range(self):
+        mpge_combined_list = self.trims.filter(mpge_combined__isnull=False).values_list('mpge_combined', flat=True)
+        return (min(mpge_combined_list), max(mpge_combined_list)) if mpge_combined_list else (None, None)
+
+    @property
+    def range_city_range(self):
+        range_city_list = self.trims.filter(range_city__isnull=False).values_list('range_city', flat=True)
+        return (min(range_city_list), max(range_city_list)) if range_city_list else (None, None)
+
+    @property
+    def range_highway_range(self):
+        range_highway_list = self.trims.filter(range_highway__isnull=False).values_list('range_highway', flat=True)
+        return (min(range_highway_list), max(range_highway_list)) if range_highway_list else (None, None)
+
+    @property
+    def range_combined_range(self):
+        range_combined_list = self.trims.filter(range_combined__isnull=False).values_list('range_combined', flat=True)
+        return (min(range_combined_list), max(range_combined_list)) if range_combined_list else (None, None)
+
+    @property
+    def charge_time_hours_240v_range(self):
+        charge_time_hours_240v_list = self.trims.filter(charge_time_hours_240v__isnull=False).values_list('charge_time_hours_240v', flat=True)
+        return (min(charge_time_hours_240v_list), max(charge_time_hours_240v_list)) if charge_time_hours_240v_list else (None, None)
+
+    @property
+    def kwh_per_100mi_range(self):
+        kwh_per_100mi_list = self.trims.filter(kwh_per_100mi__isnull=False).values_list('kwh_per_100mi', flat=True)
+        return (min(kwh_per_100mi_list), max(kwh_per_100mi_list)) if kwh_per_100mi_list else (None, None)
+
+    @property
+    def horsepower_range(self):
+        horsepower_list = self.trims.filter(horsepower__isnull=False).values_list('horsepower', flat=True)
+        return (min(horsepower_list), max(horsepower_list)) if horsepower_list else (None, None)
+
+    @property
+    def torque_range(self):
+        torque_list = self.trims.filter(torque__isnull=False).values_list('torque', flat=True)
+        return (min(torque_list), max(torque_list)) if torque_list else (None, None)
+
+    @property
+    def zero_to_sixty_seconds_range(self):
+        zero_to_sixty_seconds_list = self.trims.filter(zero_to_sixty_seconds__isnull=False).values_list('zero_to_sixty_seconds', flat=True)
+        return (min(zero_to_sixty_seconds_list), max(zero_to_sixty_seconds_list)) if zero_to_sixty_seconds_list else (None, None)
+
+    @property
+    def starting_msrp_range(self):
+        starting_msrp_list = self.trims.filter(starting_msrp__isnull=False).values_list('starting_msrp', flat=True)
+        return (min(starting_msrp_list), max(starting_msrp_list)) if starting_msrp_list else (None, None)
+
 
 class Trim(models.Model):
     model_year = models.ForeignKey(ModelYear, on_delete=models.CASCADE, related_name='trims')
